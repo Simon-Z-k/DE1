719ee02c9f64ddde4bed43aa347c700e02ecf4c8
Simon-Z-k <simon.zvoncak@gmail.com>
2025-04-25 12:10:42 +0200

    Add files via upload

---
+++ Dig_clock/Dig_clock.srcs/sources_1/new/seg7_driver.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity seg7_driver is
+--  Port ( );
+end seg7_driver;
+
+architecture Behavioral of seg7_driver is
+
+begin
+
+
+end Behavioral;
