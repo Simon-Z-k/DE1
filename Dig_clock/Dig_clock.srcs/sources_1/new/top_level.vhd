c5da9ae6417eefdf6a905b9997d6211af03ab2cc
Simon-Z-k <simon.zvoncak@gmail.com>
2025-04-25 11:17:36 +0200

    Update top_level.vhd
    Druhá verze

--- Dig_clock/Dig_clock.srcs/sources_1/new/top_level.vhd
+++ Dig_clock/Dig_clock.srcs/sources_1/new/top_level.vhd
@@ -1,124 +1,112 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/04/2025 12:31:25 PM
--- Design Name: 
--- Module Name: top_level - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+-- Top-level VHDL for Nexys A7-50T: Digital Clock with Alarm and Stopwatch -- Integrates clock, alarm, stopwatch, and 7-segment display driver with BCD mapping library IEEE; use IEEE.STD_LOGIC_1164.ALL; use IEEE.NUMERIC_STD.ALL;
 
+entity top_level is Port ( CLK100MHZ  : in  STD_LOGIC;                -- 100 MHz onboard clock BTNC       : in  STD_LOGIC;                -- Center pushbutton (active high reset) BTNL       : in  STD_LOGIC;                -- Left pushbutton (stopwatch start/stop) BTNR       : in  STD_LOGIC;                -- Right pushbutton (stopwatch reset) BTND       : in  STD_LOGIC;                -- Down pushbutton (alarm set) SW         : in  STD_LOGIC_VECTOR(15 downto 0); -- Slide switches for alarm time (HH:MM:00) CA, CB, CC, CD, CE, CF, CG, DP : out STD_LOGIC; -- 7-seg segment outputs (a..g, dp) AN         : out STD_LOGIC_VECTOR(7 downto 0)   -- 7-seg digit enables (active low) ); end top_level;
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+architecture Behavioral of top_level is -- Clock divider parameter (100 MHz -> 1 Hz) constant C_1HZ_DIV : integer := 100_000_000;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+-- Internal reset and tick signal
+signal rst       : STD_LOGIC := '0';
+signal pulse_1hz : STD_LOGIC;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-- Raw time outputs (binary)
+signal clk_h_bin, clk_m_bin, clk_s_bin : unsigned(5 downto 0);
+signal swatch_h_bin, swatch_m_bin, swatch_s_bin : unsigned(5 downto 0);
+signal alarm_on : STD_LOGIC;
 
-entity top_level is
-    Port ( CLK100MHZ : in STD_LOGIC;
-           CA : out STD_LOGIC;
-           CB : out STD_LOGIC;
-           CC : out STD_LOGIC;
-           CD : out STD_LOGIC;
-           CE : out STD_LOGIC;
-           CF : out STD_LOGIC;
-           CG : out STD_LOGIC;
-           DP : out STD_LOGIC;
-           AN : out STD_LOGIC_VECTOR (7 downto 0);
-           BTNC : in STD_LOGIC);
-end top_level;
+-- BCD digits for display (6 digits: H tens, H units, M tens, M units, S tens, S units)
+signal h10, h1, m10, m1, s10, s1 : unsigned(3 downto 0);
+signal disp_data : STD_LOGIC_VECTOR(47 downto 0);
 
-architecture Behavioral of top_level is
+begin -- Reset sync rst <= BTNC;
 
-component  clock_enable is
-    generic (
-        N_PERIODS : integer := 6
+-- Clock divider: produce 1 Hz pulse
+u_clk_en_1hz: entity work.clock_enable
+    generic map ( MAX => C_1HZ_DIV )
+    port map (
+        clk       => CLK100MHZ,
+        rst       => rst,
+        pulse_1hz => pulse_1hz
     );
-    Port ( clk : in STD_LOGIC;
-           rst : in STD_LOGIC;
-           pulse : out STD_LOGIC);
-end component;
 
-component dig_clk is
-    Port ( clk : in STD_LOGIC;
-        rst : in STD_LOGIC;
-        pulse_1hz : in STD_LOGIC; 
-        hours : out STD_LOGIC_VECTOR (4 downto 0);
-        minutes : out STD_LOGIC_VECTOR (5 downto 0);
-        seconds : out STD_LOGIC_VECTOR (5 downto 0));
-end component;
+-- Digital clock: updates binary time
+u_dig_clk: entity work.dig_clk
+    port map (
+        clk       => CLK100MHZ,
+        rst       => rst,
+        pulse_1hz => pulse_1hz,
+        hours     => std_logic_vector(clk_h_bin(5 downto 1)),
+        minutes   => std_logic_vector(clk_m_bin),
+        seconds   => std_logic_vector(clk_s_bin)
+    );
 
-component alarm is
-    Port ( clk : in STD_LOGIC;
-           rst : in STD_LOGIC;
-           set : in STD_LOGIC;
-           sw_h : in STD_LOGIC_VECTOR (4 downto 0);
-           sw_m : in STD_LOGIC_VECTOR (5 downto 0);
-           sw_s : in STD_LOGIC_VECTOR (5 downto 0);
-           current_h : in STD_LOGIC_VECTOR (4 downto 0);
-           current_m : in STD_LOGIC_VECTOR (5 downto 0);
-           current_s : in STD_LOGIC_VECTOR (5 downto 0);
-           alarm_on : out STD_LOGIC := '0');
-end component;
+-- Alarm set via switches
+u_alarm: entity work.alarm
+    port map (
+        clk       => CLK100MHZ,
+        rst       => rst,
+        set       => BTND,
+        sw_h      => std_logic_vector("00" & SW(15 downto 11)),
+        sw_m      => SW(10 downto 5),
+        sw_s      => std_logic_vector("000" & SW(4 downto 0)),
+        current_h => std_logic_vector(clk_h_bin(5 downto 1)),
+        current_m => std_logic_vector(clk_m_bin),
+        current_s => std_logic_vector(clk_s_bin),
+        alarm_on  => alarm_on
+    );
 
-component stopwatch is
-    Port ( clk : in STD_LOGIC;
-           rst : in STD_LOGIC;
-           pulse_1hz : in STD_LOGIC;
-           start_stop : in STD_LOGIC;
-           zero : in STD_LOGIC; 
-           hours : out STD_LOGIC_VECTOR (4 downto 0);
-           minutes : out STD_LOGIC_VECTOR (5 downto 0);
-           seconds : out STD_LOGIC_VECTOR (5 downto 0));
-end component;
+-- Stopwatch
+u_stopwatch: entity work.stopwatch
+    port map (
+        clk         => CLK100MHZ,
+        rst         => rst,
+        pulse_1hz   => pulse_1hz,
+        start_stop  => BTNL,
+        zero        => BTNR,
+        hours       => std_logic_vector(swatch_h_bin(5 downto 1)),
+        minutes     => std_logic_vector(swatch_m_bin),
+        seconds     => std_logic_vector(swatch_s_bin)
+    );
 
-constant C_NBITS : integer := 4;
-constant C_NPERIODS : integer := 100000000;
-
-signal sig_en_1s : std_logic;
-signal sig_count : std_logic_vector(C_NBITS-1 downto 0);
-
+-- Binary to BCD conversion for each time element
+conv_proc: process(clk100mhz)
 begin
+    if rising_edge(CLK100MHZ) then
+        if rst = '1' then
+            h10 <= (others => '0'); h1 <= (others => '0');
+            m10 <= (others => '0'); m1 <= (others => '0');
+            s10 <= (others => '0'); s1 <= (others => '0');
+        else
+            -- Hours
+            h10 <= to_unsigned(to_integer(clk_h_bin) / 10, 4);
+            h1  <= to_unsigned(to_integer(clk_h_bin) mod 10, 4);
+            -- Minutes
+            m10 <= to_unsigned(to_integer(clk_m_bin) / 10, 4);
+            m1  <= to_unsigned(to_integer(clk_m_bin) mod 10, 4);
+            -- Seconds
+            s10 <= to_unsigned(to_integer(clk_s_bin) / 10, 4);
+            s1  <= to_unsigned(to_integer(clk_s_bin) mod 10, 4);
+        end if;
+    end if;
+end process;
 
-CLKEN : clock_enable
-    generic map(
-    N_PERIODS=>C_NPERIODS
-    )
-    port map(
-        clk => CLK100MHZ,
-        rst => BTNC,
-        pulse => sig_en_1s
+-- Pack BCD digits into disp_data: [H10,H1,M10,M1,S10,S1] each 4 bits
+disp_data <= std_logic_vector(h10) & std_logic_vector(h1)
+           & std_logic_vector(m10) & std_logic_vector(m1)
+           & std_logic_vector(s10) & std_logic_vector(s1);
+
+-- 7-segment display driver
+u_seg7: seg7_driver
+    port map (
+        clk    => CLK100MHZ,
+        rst    => rst,
+        digits => disp_data,
+        seg    => (CA, CB, CC, CD, CE, CF, CG),
+        anode  => AN(5 downto 0),
+        dp     => DP
     );
-    
-DGCLK : dig_clk
 
-    generic map(
-        N_BITS=>C_NBITS
-        )
-        port map(
-            clk => CLK100MHZ,
-            rst => BTNC,
-            en => sig_en_250ms,
-            count => sig_count
-        );
-        
-        DP <='1';
-        AN <= b"01111111";
+-- Disable unused upper two digits (active low)
+AN(7 downto 6) <= "11";
+
 end Behavioral;
+
