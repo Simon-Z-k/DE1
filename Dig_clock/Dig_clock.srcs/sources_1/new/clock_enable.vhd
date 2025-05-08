017686e9553f13a379b3e7eed754874f1438d9de
Simon-Z-k <simon.zvoncak@gmail.com>
2025-05-08 21:26:16 +0200

    Update clock_enable.vhd

--- Dig_clock/Dig_clock.srcs/sources_1/new/clock_enable.vhd
+++ Dig_clock/Dig_clock.srcs/sources_1/new/clock_enable.vhd
@@ -1,55 +1,48 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+-- Deklarace entity (rozhraní modulu)
 entity clock_enable is
  generic (
+        -- MAX 100 000 000 pro 100 MHz → 1Hz)
         MAX : integer := 100000000
     );
- Port ( clk : in STD_LOGIC;
-        rst : in STD_LOGIC;
-        pulse_1hz : out STD_LOGIC);
+ Port ( 
+        clk : in STD_LOGIC;            -- Hlavní hodinový signál (100 MHz)
+        rst : in STD_LOGIC;            -- reset
+        pulse_1hz : out STD_LOGIC      -- Výstupní signál – sekunda
+    );
 end clock_enable;
 
+
 architecture Behavioral of clock_enable is
+
+    -- Čítač, který se inkrementuje až do hodnoty MAX
     signal sig_count : integer range 0 to MAX := 0;
+
+    -- Interní signál - jeden hodinový impulz
     signal pulse : STD_LOGIC := '0';
+
 begin
-    --! Count the number of clock pulses from zero to MAX.
+
+    -- Proces, který běží při každém náběžné hraně hlavního hodinového signálu (clk)
     p_clk_enable : process (clk) is
     begin
-    
-        if rst='1' then
-                sig_count <= 0;
-                pulse <='0';
-        -- Synchronous proces
-        elsif (rising_edge(clk)) then
-            -- if high-active reset then
-                -- Clear integer counter
-            if sig_count = MAX then
-                sig_count <= 0;
-                pulse <='1';
-
-            -- elsif sig_count is less than MAX then
-                -- Counting  
+        if rst='1' then                  -- Pokud je aktivní reset
+            sig_count <= 0;              
+            pulse <= '0';                
+        elsif (rising_edge(clk)) then    -- Jinak při náběžné hraně hodin
+            if sig_count = MAX then      
+                sig_count <= 0;          -- Reset čítače
+                pulse <= '1';            -- Vygenerujeme impulz – trvá jeden takt
             else
-                sig_count <= sig_count +1;
-                pulse <='0';
+                sig_count <= sig_count + 1; -- Inkrementujeme čítač
+                pulse <= '0';               -- Výstupní impulz zůstává neaktivní
             end if;
         end if;
-
     end process p_clk_enable;
 
-    -- Generated pulse is always one clock long
-    -- when sig_count = N_PERIODS-1
+    -- Připojení vnitřního signálu na výstupní port – impulz trvá vždy jeden takt (10 ns při 100 MHz)
     pulse_1hz <= pulse;
 
 end Behavioral;
