0b019de93429ade0e7fa8f41fd56565da5c94225
Simon-Z-k <simon.zvoncak@gmail.com>
2025-05-08 19:20:33 +0200

    Update clock_enable.vhd

--- Dig_clock/Dig_clock.srcs/sources_1/new/clock_enable.vhd
+++ Dig_clock/Dig_clock.srcs/sources_1/new/clock_enable.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/14/2025 11:49:15 AM
--- Design Name: 
--- Module Name: clock_enable - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
